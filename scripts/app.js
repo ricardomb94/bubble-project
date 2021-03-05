d1c32f9353d5fd2b5b66709eb1931e7d43da7c7e
ricardomb94 <aymarmb@gmail.com>
2021-03-05 23:08:17 +0100

    Added a bit of JS for animation

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// Navbar
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -8,3 +9,15 @@ hamburger.addEventListener("click", () => {
     link.classList.toggle("fade");
   });
 });
+
+// Form
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener('click',() =>{
+  container.classList.add("sign-up-mode");
+})
+sign_in_btn.addEventListener('click',() =>{
+  container.classList.remove("sign-in-mode");
+})
